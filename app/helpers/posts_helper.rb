0e6935e92f441fe4cd233616b94d15c87f6ad878
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-16 20:59:23 +0500

    Move logic from view to the helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+    def sign_in_navbar_post
+        if user_signed_in?
+            link_to('New Post', root_path, class: 'button is-info is-inverted')
+        end
+    end
+
+    def sign_in_navbar1
+        if user_signed_in?
+            link_to(current_user.name, edit_user_registration_path, class: "button is-info")
+        else
+            link_to('Sign Up', new_user_registration_path, class: "button is-info")
+        end
+    end
+    def sign_in_navbar2
+        if user_signed_in?
+            link_to("Logout", destroy_user_session_path, method: :delete, class:"button is-info")
+        else
+            link_to('Sign In', new_user_session_path, class: "button is-info")
+        end
+    end
 end
